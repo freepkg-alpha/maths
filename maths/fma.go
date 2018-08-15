515f21b8798b20d540544ad535c1f64b8cc63fd8
Amy <amyadzuki@gmail.com>
2018-08-15 13:28:50 +0000

    Create fma.go

---
+++ maths/fma.go
@@ -0,0 +1,9 @@
+package maths
+
+func Fma(base, mul, add float64) float64 {
+	return base * mul + add
+}
+
+func FmaFloat32(base, mul, add float32) float32 {
+	return float32(Fma(float64(base), float64(mul), float64(add)))
+}
